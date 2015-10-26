8ac567ea03a6f7204ec464d013dc3178ee855457
jcheunga <jasoncheunga@gmail.com>
2015-10-26 19:56:42 +1100

    initial

---
+++ regex.js
@@ -0,0 +1,21 @@
+// USEFUL regex patterns
+
+// url validation
+
+/*eslint-disable */
+const isurl = {
+	urlcheck: function(url) {
+		let myRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+		if (myRegExp.test(url)) {
+			return true;
+		}
+	}
+};
+
+export default isurl;
+// \d to \\d http://stackoverflow.com/questions/17510672/url-thats-makes-the-regular-expression-test-to-freeze
+
+
+// price to number
+
+Number(price.replace(/[^0-9\.-]+/g,""))
